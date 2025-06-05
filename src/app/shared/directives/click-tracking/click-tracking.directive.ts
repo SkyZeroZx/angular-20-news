aeb8b01fca582767690e0df52c93968a15f02434
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-06-04 23:49:14 -0500

    feat: Add modal service

--- src/app/shared/directives/click-tracking/click-tracking.directive.ts
+++ src/app/shared/directives/click-tracking/click-tracking.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input, output } from '@angular/core';
 
 @Directive({
   selector: '[appClickTracking]',
@@ -7,7 +7,12 @@ import { Directive } from '@angular/core';
   },
 })
 export class ClickTrackingDirective {
+  customEvent = input<unknown>();
+
+  trackingEvent = output<unknown>();
+
   trackingClick() {
-    console.log('Tracking Click ...');
+    console.log('Tracking Click ...', { custom: this.customEvent() });
+    this.trackingEvent.emit('Tracking Event ' + this.customEvent());
   }
 }
