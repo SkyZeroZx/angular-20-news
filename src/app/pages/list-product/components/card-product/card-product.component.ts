799293ba80cb9d807dce8701a76321b6b69a8b06
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-07-04 12:25:37 -0500

    feat: Add product details modal service

--- src/app/pages/list-product/components/card-product/card-product.component.ts
+++ src/app/pages/list-product/components/card-product/card-product.component.ts
@@ -12,6 +12,11 @@ import { RouterLink } from '@angular/router';
 import { ProductCard } from '../../../../core/interfaces';
 import { ShopCartService } from '../../../../services/shop-cart';
 import { ClickTrackingDirective } from '../../../../shared/directives/click-tracking/click-tracking.directive';
+import {
+  ModalModule,
+  ModalService,
+} from '../../../../shared/ui/services/modal';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @Component({
   selector: 'app-card-product',
@@ -21,6 +26,7 @@ import { ClickTrackingDirective } from '../../../../shared/directives/click-trac
     NgOptimizedImage,
     RouterLink,
     ClickTrackingDirective,
+    ModalModule,
   ],
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.scss',
@@ -28,12 +34,19 @@ import { ClickTrackingDirective } from '../../../../shared/directives/click-trac
 })
 export class CardProductComponent {
   private readonly shopCartService = inject(ShopCartService);
+  private readonly modalService = inject(ModalService);
   readonly product = input.required<ProductCard>();
 
   addToCard() {
     this.shopCartService.add({
       ...this.product(),
       quantity: 1,
+    });
+  }
+
+  showDetails() {
+    this.modalService.open(ProductDetailsComponent, {
+      data: this.product(),
     });
   }
 }
