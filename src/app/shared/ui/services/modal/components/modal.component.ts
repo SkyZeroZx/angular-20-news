373bc9eca21455f0cae88cf98ad5c54d26368086
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-07-04 12:26:45 -0500

    feat: update modal

--- src/app/shared/ui/services/modal/components/modal.component.ts
+++ src/app/shared/ui/services/modal/components/modal.component.ts
@@ -1,83 +1,78 @@
 import { DOCUMENT, NgClass, NgStyle } from '@angular/common';
 import {
-	ChangeDetectionStrategy,
-	Component,
-	ElementRef,
-	EventEmitter,
-	Inject,
-	ModelSignal,
-	OnInit,
-	Output,
-	Renderer2,
-	computed,
-	model,
-	signal
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ModelSignal,
+  OnInit,
+  Renderer2,
+  computed,
+  inject,
+  model,
+  output,
+  signal,
 } from '@angular/core';
 import { FADE_ANIMATION_DELAY } from '../constants/animation.constant';
 import { ModalService } from '../services/modal.service';
 
 @Component({
-	selector: 'app-modal',
-	standalone: true,
-	imports: [NgClass, NgStyle],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	templateUrl: './modal.component.html',
-	styleUrls: ['./modal.component.scss']
+  selector: 'app-modal',
+  imports: [NgClass, NgStyle],
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class ModalComponent implements OnInit {
-	protected show = signal<boolean>(true);
+  private readonly modalHost = inject(ElementRef);
+  private readonly document = inject<Document>(DOCUMENT);
+  private readonly modalService = inject(ModalService);
+  private readonly renderer2 = inject(Renderer2);
 
-	mode: ModelSignal<'dialog' | 'modal' | 'bottom-sheet'> = model('modal');
-	// This is required since ngStyle is using `any` as well
-	// More details in https://angular.io/api/common/NgStyle
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	style = model<{ [k: string]: any }>({});
+  protected show = signal<boolean>(true);
 
-	setting = computed(() => ({
-		isModal: this.mode() === 'modal',
-		isDialog: this.mode() === 'dialog',
-		isBottomSheet: this.mode() === 'bottom-sheet'
-	}));
+  mode: ModelSignal<'dialog' | 'modal' | 'bottom-sheet'> = model('modal');
+  // This is required since ngStyle is using `any` as well
+  // More details in https://angular.io/api/common/NgStyle
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  style = model<{ [k: string]: any }>({});
 
-	@Output()
-	closed = new EventEmitter<void>();
+  setting = computed(() => ({
+    isModal: this.mode() === 'modal',
+    isDialog: this.mode() === 'dialog',
+    isBottomSheet: this.mode() === 'bottom-sheet',
+  }));
 
-	//	private elementSticky: HTMLElement;
-	private htmlBaseElement!: HTMLHtmlElement;
+  closed = output<void>();
 
-	constructor(
-		private readonly modalHost: ElementRef<HTMLElement>,
-		@Inject(DOCUMENT) private readonly document: Document,
-		private readonly renderer2: Renderer2,
-		private readonly modalService: ModalService
-	) {}
+  //	private elementSticky: HTMLElement;
+  private htmlBaseElement!: HTMLHtmlElement;
 
-	ngOnInit(): void {
-		this.hiddenScroll();
-	}
+  ngOnInit(): void {
+    this.hiddenScroll();
+  }
 
-	private hiddenScroll() {
-		this.htmlBaseElement = this.document.getElementsByTagName('html')[0];
-		this.htmlBaseElement.style.overflowY = 'hidden';
-	}
+  private hiddenScroll() {
+    this.htmlBaseElement = this.document.getElementsByTagName('html')[0];
+    this.htmlBaseElement.style.overflowY = 'hidden';
+  }
 
-	private addScroll() {
-		if (this.modalService.listModalComponentRef.length === 0) {
-			this.renderer2.setStyle(this.htmlBaseElement, 'overflowY', 'visible');
-		}
-	}
+  private addScroll() {
+    if (this.modalService.listModalComponentRef.length === 0) {
+      this.renderer2.setStyle(this.htmlBaseElement, 'overflowY', 'visible');
+    }
+  }
 
-	open() {
-		this.show.set(true);
-	}
+  open() {
+    this.show.set(true);
+  }
 
-	close() {
-		this.show.set(false);
+  close() {
+    this.show.set(false);
 
-		setTimeout(() => {
-			this.closed.emit();
-			this.modalHost.nativeElement.remove();
-			this.addScroll();
-		}, FADE_ANIMATION_DELAY);
-	}
+    setTimeout(() => {
+      this.closed.emit();
+      this.modalHost.nativeElement.remove();
+      this.addScroll();
+    }, FADE_ANIMATION_DELAY);
+  }
 }
